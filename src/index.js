45163e467434030a5fd699ce051484fc3ba430d7
jonideborger <56124097+jonideborger@users.noreply.github.com>
2022-03-18 16:39:25 +0100

    added tfvis as a dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
 import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
 
-console.log(tf)
+console.log(tf);
+console.log(tfvis);
+console.log('Wathcing?')
